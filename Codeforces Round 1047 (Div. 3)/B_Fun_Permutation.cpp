c1a475280e9a4c8cc54c1d51b79be268efbacdc0
TONMOY, WASIMUL BARI <punpunonodera754@gmail.com>
2025-09-10 21:39:51 +0600

    code.

--- Codeforces Round 1047 (Div. 3)/B_Fun_Permutation.cpp
+++ Codeforces Round 1047 (Div. 3)/B_Fun_Permutation.cpp
@@ -1,32 +1,32 @@
 #include <iostream>
+#include <vector>
+
 using namespace std;
 
-int main() {
+int main()
+{
     int t;
     cin >> t;
 
-    while (t--) {
+    while (t--)
+    {
         int n;
         cin >> n;
-
-        int p[1000];
-        int q[1000];
-
-        for (int i = 0; i < n; i++) {
+        vector<int> p(n);
+        for (int i = 0; i < n; i++)
+        {
             cin >> p[i];
         }
 
-        for (int i = 0; i < n; i++) {
-            q[i] = (n + 1) - p[i];
+        vector<int> q(n);
+        for(int i = 0; i < n; i++) {
+            q[i] = n+1-p[i];
         }
 
-        // print q
-        for (int i = 0; i < n; i++) {
-            cout << q[i];
-            if (i < n - 1) cout << " ";
+        for(int i = 0; i < n; i++) {
+            cout << q[i] << " ";
         }
-        cout << "\n";
+        cout << endl;
     }
-
     return 0;
 }
